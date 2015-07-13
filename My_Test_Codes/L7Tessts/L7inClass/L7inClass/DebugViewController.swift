88e04d217b05cace84639dc80d5f94a5e7918204
Peng He <phe@wpi.edu>
2015-07-13 19:48:30 -0400

    Add Lc7 in class demo

---
+++ My_Test_Codes/L7Tessts/L7inClass/L7inClass/DebugViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DebugViewController.swift
+//  L7inClass
+//
+//  Created by Peng on 7/13/15.
+//  Copyright (c) 2015 Peng. All rights reserved.
+//
+
+import UIKit
+
+class DebugViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
