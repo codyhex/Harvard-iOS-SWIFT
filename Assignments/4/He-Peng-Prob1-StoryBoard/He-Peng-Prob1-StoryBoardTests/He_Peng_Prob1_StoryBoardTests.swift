6ec0ed6270ae5fa290f7a124d3080f399a2be8bb
He, Peng <penghe@g.harvard.edu>
2015-07-21 12:08:45 -0400

    Add the CCB UI
    - Five views storyboard

---
+++ Assignments/4/He-Peng-Prob1-StoryBoard/He-Peng-Prob1-StoryBoardTests/He_Peng_Prob1_StoryBoardTests.swift
@@ -0,0 +1,36 @@
+//
+//  He_Peng_Prob1_StoryBoardTests.swift
+//  He-Peng-Prob1-StoryBoardTests
+//
+//  Created by HePeng on 7/21/15.
+//  Copyright (c) 2015 HePeng. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class He_Peng_Prob1_StoryBoardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
