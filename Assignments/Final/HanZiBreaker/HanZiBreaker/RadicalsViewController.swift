9aa3a9017f4d6af9c4445a7ee93e054b9e3febc1
Peng He <phe@wpi.edu>
2015-08-03 02:04:29 -0400

    + Draw

--- Assignments/Final/HanZiBreaker/HanZiBreaker/RadicalsViewController.swift
+++ Assignments/Final/HanZiBreaker/HanZiBreaker/RadicalsViewController.swift
@@ -14,18 +14,98 @@ class RadicalsViewController: UIViewController {
     var chineseName: String?
     var FCCode: String?
     
+    @IBOutlet weak var selectView: UIImageView!
+    //for draw
+    var swiped = false
+    var lastPoint = CGPoint.zeroPoint
+    var red: CGFloat = 1.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    
     @IBOutlet weak var radicalsTextField: UILabel! {
         didSet {
             radicalsTextField.text = "Selected Radical Code: \(FCCode!)"
         }
     }
     
+    
+    //draw
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = false
+        if let touch = touches.first as? UITouch {
+            lastPoint = touch.locationInView(self.view)
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        selectView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        // 2
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        // 3
+        CGContextSetLineCap(context, kCGLineCapRound)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, kCGBlendModeNormal)
+        
+        // 4
+        CGContextStrokePath(context)
+        
+        // 5 draw to
+        selectView.image = UIGraphicsGetImageFromCurrentImageContext()
+        selectView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        // 6
+        swiped = true
+        if let touch = touches.first as? UITouch {
+            let currentPoint = touch.locationInView(view)
+            drawLineFrom(lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(lastPoint, toPoint: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        //UIGraphicsBeginImageContext(selectView.frame.size)
+        //selectView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
+        //selectView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: opacity)
+        //selectView.image = UIGraphicsGetImageFromCurrentImageContext()
+        //UIGraphicsEndImageContext()
+        
+       // tempImageView.image = nil
+    }
+
+    
+    
     // MARK: - Navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == Identifiers.possibleRadicalsSegue {
             if let moreDetailVC = segue.destinationViewController as? UIViewController {
                 if let navVC = parentViewController as? UINavigationController {
+                    
                     let numControllersOnStack = navVC.viewControllers.count
+                    
                     if let tableVC = navVC.viewControllers[numControllersOnStack - 2] as? IndexTableViewController {
                         tableVC.mostRecentMoreInfo = identifier
                         moreDetailVC.title = "Possible Radicals"
